9bbb2c0b6c17e33cc02941d0bf0d93eae1ecc580
Srijan <srishivmgs1014@gmail.com>
2025-11-08 07:58:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Fade-in on scroll animation
-const fadeEls = document.querySelectorAll('.fade-in');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('visible');
-  });
-});
-fadeEls.forEach(el => observer.observe(el));
